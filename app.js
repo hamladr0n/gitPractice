6c4edc3eb8d2d760d8fcf114d36d8af4166b7fbf
Corbin Prebe <cjprebe@gmail.com>
2022-03-02 16:20:15 -0800

    updating udemy course work

---
+++ app.js
@@ -0,0 +1,78 @@
+// let random = Math.random();
+
+
+// if (random < 0.5) {
+//     console.log("Your number is less than 0.5")
+//     console.log(random);
+// }
+
+// let num = 3;
+
+// function isEven(num) { return num % 1;}
+//     console.log("even");
+
+// const number = 13;
+
+// const isOdd = number % 2 !== 0;
+
+// console.log(isOdd);
+
+// let num = Math.random() * 10;
+
+
+// console.log(Math.round(num));
+// if (num % 2 === 0) {
+//     console.log("even")
+// }
+// console.log("before");
+
+// let random = Math.random() * 10;
+
+// console.log("random is " + random);
+
+// // num = Math.round(random);
+// console.log("before");
+// const num = 2
+// const num2 = 3
+// function isEven(num) {
+//   let n = num
+//   console.log("during");
+//   return isInteger(num) && (num % 2 !== 0);
+// }
+
+// function oddOrEven(num2){
+//   if(num2 % 2 == 0)
+//       return "even";
+//   return "odd";
+// }
+// oddOrEven(num2);
+
+
+// console.log("after");
+
+// function isEven(random) {
+//     if (random === isEven){
+//         console.log("even");
+//     }
+// }
+// function isEven(num) {
+//     console.log("xxx");
+//     console.log(num);
+// }
+
+// if (num == isEven()) {
+//     console.log("odd");
+// } else {
+//     console.log("even");
+// }
+
+// console.log("after");
+
+
+function isEven(num){
+  //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+ if(num % 2 == 0){
+     console.log("even");
+ } 
+  //AND THIS LINE ↑↑↑↑↑
+}
---
+++ jselseif.js
@@ -0,0 +1,57 @@
+// const dayOfWeek = prompt("choose a day of the week").toLowerCase();
+
+// if (dayOfWeek === 'monday') {
+//     console.log("UGHHH I HATE MONDAYS!")
+// } else if (dayOfWeek === 'tuesday'){
+//     console.log("YAY I LOVE Tuesday!")
+// } else if (dayOfWeek === 'wednesday'){
+//     console.log("YAY I LOVE Wednesday!")
+// } else if (dayOfWeek === 'thursday'){
+//     console.log("YAY I LOVE Thursday!")
+// } else if (dayOfWeek === 'friday'){
+//     console.log("YAY I LOVE Friday!")
+// } else if (dayOfWeek === 'saturday'){
+//     console.log("YAY I LOVE SATURDAYS!")
+// } else if (dayOfWeek === 'sunday'){
+//     console.log("YAY I LOVE SundayS!")
+// } else {
+//     console.log("meh")
+// }
+
+// 0-5 - FREE -5-10 
+// CHILD $10
+// 10 - 65 ADULT $20
+// 65+ SENIOR $10
+
+const age = 8;
+
+if (age < 5){
+    console.log("you are a baby. you get in free!")
+} else if (age< 10){
+    console.log("you are a child. you pay $10")
+} else if (age < 65){
+    console.log("you are an adult. you pay $20")
+}
+
+
+let random = Math.random();
+if (random < 0.5) {
+    console.log("your number is less than 0.5")
+    console.log(random);
+} else {
+    console.log("your number is greater than 0.5");
+}
+
+console.log(random);
+
+// Exercise Criteria for Nesting Demo
+//Pass must be 6+ characters
+//pass cannot include a space 
+
+const password = prompt("please enter a new password");
+
+if (password.length >= 6 ) {
+    console.log("Long enough password")
+} else {
+    console.log("password too short! must be 6+ character");
+}
