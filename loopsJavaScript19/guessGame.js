3bfb39ffb67460bf116a8be05613f3ccc1c88b06
Corbin Prebe <cjprebe@gmail.com>
2022-03-04 15:12:10 -0800

    more class

--- loopsJavaScript19/guessGame.js
+++ loopsJavaScript19/guessGame.js
@@ -1,13 +1,22 @@
-// console.log(1)
-// console.log(2)
-// console.log(3)
-// console.log(4)
-// console.log(5)
-// console.log(6)
-// console.log(7)
-// console.log(8)
-// console.log(9)
+let maximum = parseInt(prompt("Enter the maximum number!"));
+while(!maximum) {
+    maximum =parseInt(prompt("Enter a valid number!"));
+}
 
-// for(let i = 1; i <=10; i++) {
-//     console.log(i);
-// }
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+
+let guess = parseInt(prompt("Enter your first guess!!"));
+let attempts = 1;
+
+
+while (parseInt(guess) !== targetNum){
+    if (guess === 'q') break;
+    attempts++;
+    if(guess > targetNum) {
+        guess = prompt("Too High! Enter a new guess:");
+    } else {
+        guess = prompt("Too Low! Enter a new guess:")
+    }
+}
+console.log(`You got it! it was: ${targetNum}! It took you ${attempts} attempts`)
