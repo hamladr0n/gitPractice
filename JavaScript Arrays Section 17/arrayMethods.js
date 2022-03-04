71c102db082fc6ff46a1c75572a983e1e63aab85
Corbin Prebe <cjprebe@gmail.com>
2022-03-03 17:20:07 -0800

    arrays + const, nested arrays

---
+++ JavaScript Arrays Section 17/arrayMethods.js
@@ -0,0 +1,38 @@
+// const array1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h '];
+// const array2 = [ 'i', 'j', 'k', 'l', 'm', 'n'];
+// const array3 = array1.concat(array2);
+
+// console.log(array3);
+
+
+// (14) ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h ', 'i', 'j', 'k', 'l', 'm', 'n']
+// array2
+// (6) ['i', 'j', 'k', 'l', 'm', 'n']
+// let cats = ['bblue', 'fred', 'bob']
+// undefined
+// let dogs = ['leo', 'max']
+// undefined
+// cats
+// (3) ['bblue', 'fred', 'bob']
+// dogs
+// (2) ['leo', 'max']
+// let comboParty = dogs.concat(cats)
+// undefined
+// comboParty
+// (5) ['leo', 'max', 'bblue', 'fred', 'bob']
+// cats.includes('bob')
+// true
+// cats.includes('Bob')
+// false
+// "Bob".indexOf('B'0
+// VM4432:1 Uncaught SyntaxError: missing ) after argument list
+// "Bob".indexOf('B')
+// 0
+// "Bob".indexOf('o')
+// 1
+// comboParty.indexOf('max')
+// 1
+// comboParty.indexOf('asdfasdf')
+// -1
+// comboParty.reverse()
+// (5) ['bob', 'fred', 'bblue', 'max', 'leo']
---
+++ JavaScript Arrays Section 17/arraysConst.js
@@ -0,0 +1,18 @@
+const PI = 3.14159;
+
+//PI += 1
+const nums = [1, 2, 3]
+
+nums.push(4);
+nums
+
+const board = [ [['X', 'O','X'], ['0', null, 'X'], ['O', 'O','X']]]
+
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
